baead1cc0e9750988aa49a315680988a8c4e3fee
M Abulkhair <mr.abulkhair.x1@gmail.com>
2023-06-20 22:02:08 +0300

    some more edits

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -6,13 +6,15 @@
 #include <stdio.h>
 #include <string.h>
 
+#define FILE_NAME __FILE__
+
 /**
  * main - Print file name
  * Return: 0
  */
 int main(void)
 {
-    char name[5000] = __FILE__;
+    char name[5000] = FILE_NAME;
     puts(name);
 
     return (0);
