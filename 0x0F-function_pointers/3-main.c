e9fc5455465845136c38d00fdfdda74aa4a4e26b
M Abulkhair <mr.abulkhair.x1@gmail.com>
2023-06-22 03:09:52 +0300

    finalized 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+/*
+ * File: 3-main.c
+ * Auth: Mohammad Abulkhair
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - receives 4 arguments from command line and performs operations
+ * @argc: command line arguments count
+ * @argv: array of strings of arguments
+ * 
+ * Return: 98 for wrong number of arguments. 99 for wrong operator. 
+ * 100 for invalid division. otherwise result of operation.
+ */
+
+int main(int argc, char *argv[])
+{
+    int (*f)(int a, int b) = get_op_func(argv[2]);
+    int a = atoi(argv[1]);
+    int b = atoi(argv[3]);
+
+    if (argc != 4)
+    {
+        puts("Error");
+        return 98;
+    }
+
+    if (op == NULL)
+    {
+        puts("Error");
+        return 99;
+    }
+
+    if ((argv[2] == '/' || argv[2] == '/') && b == 0)
+    {
+        puts("Error");
+        return 100;
+    }
+
+    return f(a, b);
+}
